de84be8994374cff83685ed2a75a5bdd63d87539
Derek Blankenship <derek@nyghtly.net>
2019-03-16 14:19:47 -0400

    Remove unused variable

--- src/sys/ender.rs
+++ src/sys/ender.rs
@@ -7,11 +7,10 @@ impl<'a> System<'a> for Ender {
     type SystemData = (
         ReadExpect<'a, r::KeysPressed>,
         WriteExpect<'a, r::KillProgram>,
-        ReadExpect<'a, r::GameMap>,
     );
 
     fn run(&mut self, data: Self::SystemData) {
-        let (keys, mut kill, map) = data;
+        let (keys, mut kill) = data;
         if keys.escape {
             kill.0 = true;
         }
